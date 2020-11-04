5091c2a38e5addc7ef3d37bdd2af8decec6319e0
RamanaKuruba <72610420+RamanaKuruba@users.noreply.github.com>
2020-11-04 17:32:58 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -9,10 +9,32 @@ public class HelloWorld {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
     String s1 = new String("test");
-    int i =10;
+    int a =10;
     //int k =20;
+     try {
+    doSomethingWhichThrowsException();
+    System.out.println("OK");   // incorrect "OK" message is printed
+  } catch (RuntimeException e) {
+    System.out.println("ERROR");  // this message is not shown
+  }
 
     System.out.println("Hello, World! The current time is " + dtf.format(now));
   }
+  
+  public static void doSomethingWhichThrowsException() {
+  try {
+    throw new RuntimeException();
+  } finally {
+    for (int i = 0; i < 10; i ++) {
+      //...
+      if (q == i) {
+        break; // ignored
+      }
+    }
+
+    /* ... */
+    return;      // Noncompliant - prevents the RuntimeException from being propagated
+  }
+  
 
 }
